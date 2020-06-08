37e6b309b35ac233b3c560cdb493f7861a883990
Mallory Robertson <mallory@truss.works>
2020-06-08 16:20:16 -0700

    Finished section 7

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value);
+    state = { term: '' };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
     }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="ui field">
                         <label>Image search</label>
                         <input 
                         type="text"
-                        onChange={this.onInputChange}
-                        /> // this.onInputChange leaves out trailing () so that the function doesn't get called ever ytime we render, don't add () on callback functions!
+                        onChange={e => this.setState({ term: e.target.value })}
+                        value={this.state.term}
+                        /> 
                     </div>
                 </form>
             </div>
