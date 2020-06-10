cad81b351871dc1075c89a8e99b2435ccb6ad738
Mallory Robertson <mallory@truss.works>
2020-06-10 14:25:22 -0700

    Finished section 10

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -5,15 +5,25 @@ class ImageCard extends React.Component {
         super(props);
 
         this.imageRef = React.createRef();
+
+        this.state = { spans: 0 }
     }
 
     componentDidMount() {
-        console.log(this.imageRef)
+        this.imageRef.current.addEventListener('load', this.setSpans);
     }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10 + 1);
+        this.setState({ spans }); // spans: spans was shortened to just spans (ES2015 syntax)
+
+    }
+
     render() {
         const { description, urls } = this.props.image; // Used so that this.image.* doesn't have to be types over and over
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}`}}>
                 <img alt={description}
                 src={urls.regular}
                 ref={this.imageRef}
